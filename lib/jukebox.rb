bb1cb7d339a740be0684277d2dd223044942cc16
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-27 23:15:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,67 @@
 # Add your code here
+# def say_hello(name)
+#   "Hi #{name}!"
+# end
+ 
+# puts "Enter your name:"
+# users_name = gets.strip
+ 
+# puts say_hello(users_name)
+
+def help 
+puts "I accept the following commands:"
+puts "- help : displays this help message"
+puts "- list : displays a list of songs you can play"
+puts "- play : lets you choose a song to play"
+puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  song = gets.strip
+  if songs.include?(song) || songs[song - 1]
+    puts "Playing #{song}"
+  else 
+    puts "Invalid input, please try again"
+  end
+end 
+
+def list (songs)
+  songs.each_with_index
+end 
+
+def exit_jukebox
+  puts "Goodbye"
+end 
+
+def run(songs)
+  puts "Please enter a command:"
+  command = gets.strip
+  
+  while !!5 do 
+    
+    if command == "help"
+      1.times do 
+        help 
+      end 
+     
+    elsif command == "list"
+      1.times do 
+        list(songs)
+      end 
+    elsif command == "play"
+      1.times do 
+        play(songs)
+      end 
+    end
+    puts "Please enter a command:"
+    command = gets.strip
+    
+    break if command == "exit"
+  end 
+  
+   if command == "exit"
+     exit_jukebox
+   end
+end 
+
